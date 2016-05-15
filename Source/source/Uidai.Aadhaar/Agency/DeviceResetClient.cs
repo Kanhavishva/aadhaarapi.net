e91580e7cef7a0533c383c948ced43c4103f60b3
souvikdc9 <souvikdc@hotmail.com>
2016-05-15 23:17:39 +0530

    Add DeviceResetClient

---
+++ Source/source/Uidai.Aadhaar/Agency/DeviceResetClient.cs
@@ -0,0 +1,45 @@
+﻿#region Copyright
+/********************************************************************************
+ * Aadhaar API for .NET
+ * Copyright © 2015 Souvik Dey Chowdhury
+ * 
+ * This file is part of Aadhaar API for .NET.
+ * 
+ * Aadhaar API for .NET is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Aadhaar API for .NET is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Aadhaar API for .NET. If not, see http://www.gnu.org/licenses.
+ ********************************************************************************/
+#endregion
+
+using Uidai.Aadhaar.Api;
+using Uidai.Aadhaar.Helper;
+using static Uidai.Aadhaar.Internal.ExceptionHelper;
+
+namespace Uidai.Aadhaar.Agency
+{
+    /// <summary>
+    /// Provides a wrapper to send a registered device request and retrieve a response from CIDR servers.
+    /// </summary>
+    public class DeviceResetClient : ApiClient<DeviceResetRequest, DeviceResetResponse>
+    {
+        /// <summary>
+        /// When overridden in a descendant class, sets the <see cref="ApiClient{TRequest, TResponse}.Address"/> property.
+        /// </summary>
+        protected override void ApplyAddress()
+        {
+            ValidateNull(AgencyInfo, nameof(AgencyInfo));
+            ValidateNull(Request, nameof(Request));
+
+            Address = AgencyInfo.GetAddress(Request.ApiName);
+        }
+    }
+}
